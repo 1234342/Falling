0913261a5e0cef1c28d53d5040722b0166fb27fe
YuahP <yuah.build@gmail.com>
2015-09-13 22:04:58 +0900

    Add Setting base

--- app/src/main/java/com/kudosku/falling/MainActivity.java
+++ app/src/main/java/com/kudosku/falling/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.setting, menu);
+        getMenuInflater().inflate(R.menu.settings, menu);
         getMenuInflater().inflate(R.menu.credits, menu);
         return true;
     }
--- app/src/main/java/com/kudosku/falling/Setting.java
+++ app/src/main/java/com/kudosku/falling/Setting.java
@@ -1,14 +1,16 @@
 package com.kudosku.falling;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
 
-public class Setting extends AppCompatActivity {
+public class Setting extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting);
+        addPreferencesFromResource(R.xml.setting);
     }
 
 

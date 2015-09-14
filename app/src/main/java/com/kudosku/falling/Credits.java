7e7cd90dd78f59108a0b3bd23ee6a4a70dcf3531
YuahP <yuah.build@gmail.com>
2015-09-14 18:38:23 +0900

    Add Credit info

--- app/src/main/java/com/kudosku/falling/Credits.java
+++ app/src/main/java/com/kudosku/falling/Credits.java
@@ -1,13 +1,44 @@
 package com.kudosku.falling;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class Credits extends AppCompatActivity {
+
+    ArrayList<String> credits = new ArrayList<String>();
+    ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.credits);
+
+        credits.add("Programming : YuahP");
+        credits.add("Programming : KDPark");
+
+        ArrayAdapter adapter= new ArrayAdapter(Credits.this, android.R.layout.simple_list_item_1, credits);
+
+        listview = (ListView)findViewById(R.id.credits_list);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(listener);
+
     }
+
+    AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener() {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(Credits.this, credits.get(position), Toast.LENGTH_SHORT).show();
+        }
+
+
+    };
 }

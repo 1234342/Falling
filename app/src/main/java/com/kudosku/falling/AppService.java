99f97102a8097e66373553d4b435803db7eefa7d
YuahP <yuah.build@gmail.com>
2015-09-15 17:31:16 +0900

    Update Notification

--- app/src/main/java/com/kudosku/falling/AppService.java
+++ app/src/main/java/com/kudosku/falling/AppService.java
@@ -1,12 +1,18 @@
 package com.kudosku.falling;
 
 import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 public class AppService extends Service {
+
+    private Notification mNoti;
 
     @Override
     public void onCreate() {
@@ -15,7 +21,24 @@ public class AppService extends Service {
 
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        startForeground(1, new Notification());
+        PendingIntent mPI = PendingIntent.getActivity(
+                getApplicationContext(), 0, new Intent(getApplicationContext(),MainActivity.class),
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationManager mNty = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNoti = new NotificationCompat.Builder(getApplicationContext())
+                .setContentTitle(getResources().getString(R.string.app_name))
+                .setContentText(getResources().getString(R.string.app_touch))
+                .setSmallIcon(R.drawable.splash)
+                .setTicker(getResources().getString(R.string.app_alive))
+                .setWhen(System.currentTimeMillis())
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setAutoCancel(false)
+                .setContentIntent(mPI)
+                .build();
+
+        //mNty.notify(3939, mNoti);
+        startForeground(3939, mNoti);
         Toast.makeText(getApplicationContext(), "서비스를 시작합니다.", Toast.LENGTH_LONG).show();
 
         return Service.START_STICKY;

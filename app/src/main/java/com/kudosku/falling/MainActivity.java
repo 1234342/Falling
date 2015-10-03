92d2e6a6c36170d46707074a415e10b52e663f4e
YuahP <yuah.build@gmail.com>
2015-10-03 13:22:28 +0900

    upload

--- app/src/main/java/com/kudosku/falling/MainActivity.java
+++ app/src/main/java/com/kudosku/falling/MainActivity.java
@@ -94,7 +94,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.settings, menu);
-        getMenuInflater().inflate(R.menu.credits, menu);
         getMenuInflater().inflate(R.menu.serviceoff, menu);
         getMenuInflater().inflate(R.menu.serviceon, menu);
 

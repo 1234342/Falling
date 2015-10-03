2bceaccc3d27156215329c1185ae5489bce86d05
YuahP <yuah.build@gmail.com>
2015-10-03 13:25:27 +0900

    a

--- app/src/main/java/com/kudosku/falling/WeatherInit.java
+++ app/src/main/java/com/kudosku/falling/WeatherInit.java
@@ -1,8 +1,8 @@
 package com.kudosku.falling;
 
 public class WeatherInit {
-    String lat;
-    String ion;
+    Float lat;
+    Float ion;
     Float temperature;
     String weather;
     String cloudy;
@@ -10,16 +10,15 @@ public class WeatherInit {
     String rain;
     String city;
 
-    public void setLat(String lat){ this.lat = lat;}
-    public void setIon(String ion){ this.ion = ion;}
+    public voiFloatat(String lat){ this.lat = lat;}
+    public voiFloaton(String ion){ this.ion = ion;}
     public void setTemperature(Float t){ this.temperature = t;}
     public void setWeather(String weather){ this.weather = weather;}
     public void setCloudy(String cloudy){ this.cloudy = cloudy;}
     public void setSnow(String snow){ this.snow = snow;}
     public void setRain(String rain){ this.rain = rain;}
-    public void setCity(String city){ this.city = city;}
-
-    public String getLat(){ return lat;}
+    public void setCity(String city){ this.city = cFloat
+    public String getLat(){ returnFloat
     public String getIon() { return ion;}
     public String getWeather(){ return weather;}
     public Float getTemprature() { return temperature;}

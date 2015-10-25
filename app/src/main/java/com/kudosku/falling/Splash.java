d9f26f4d6c112540a97b4df9f4bc24f7838d2f4b
YuahP <yuah.build@gmail.com>
2015-10-25 19:07:10 +0900

    UPDATE SPLASH

--- app/src/main/java/com/kudosku/falling/Splash.java
+++ app/src/main/java/com/kudosku/falling/Splash.java
@@ -1,20 +1,32 @@
 package com.kudosku.falling;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Window;
 
-public class Splash extends AppCompatActivity {
+public class Splash extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try{
-            Thread.sleep(3000); // 1000 당 1초
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        setContentView(R.layout.splash);
 
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
+        //try{
+        //    Thread.sleep(3000); // 1000 당 1초
+        //} catch (InterruptedException e) {
+        //    e.printStackTrace();
+        //}
+
+        Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+                finish();
+            }
+        };
+
+        handler.sendEmptyMessageDelayed(0, 3000);
     }
 }
